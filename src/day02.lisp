a1d9691993771de1173300286ed4f56d05a1cb8e
Brit Butler <brit@kingcons.io>
2020-12-02 13:05:37 -0500

    Refactor day 2 part 2 using alexandria:xor.

--- src/day02.lisp
+++ src/day02.lisp
@@ -11,17 +11,18 @@
       ("(\\d+)-(\\d+) (\\w): (\\w+)" row)
     (list min max letter password)))
 
+(defun xor-match? (min max letter password)
+  (alexandria:xor (string= letter password :start2 (1- min) :end2 min)
+                  (string= letter password :start2 (1- max) :end2 max)))
+
 (defun count-valid (items)
   (loop for (min max letter password) in items
         counting (let ((count (count letter password :test #'string=)))
                    (<= min count max))))
 
 (defun count-xor-valid (items)
-  (flet ((letter-at? (letter string index)
-           (count letter string :start (1- index) :end index :test #'string=)))
-    (loop for (min max letter password) in items
-          counting (plusp (logxor (letter-at? letter password min)
-                                  (letter-at? letter password max))))))
+  (loop for (min max letter password) in items
+        counting (xor-match? min max letter password)))
 
 (defun part-1 ()
   (let ((items (read-day-input 2 #'parse-password)))
