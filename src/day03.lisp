104f1d641f5081ed39ad0430fde91890f1a4531f
Brit Butler <brit@kingcons.io>
2020-12-03 11:00:26 -0500

    Ditch circular lists, prefer modular arithmetic.
    Skipping unnecessary work makes stuff faster. Go figure!
    
    Specifically: no parsing, nthcdr + modular arithmetic over
    repeated list traversals.
    
    Before:
    
    DAY-03> (let ((map (read-day-input 3 #'parse-terrain)))
              (time (count-collisions map)))
    Evaluation took:
      0.003 seconds of real time
      0.002475 seconds of total run time (0.002470 user, 0.000005 system)
      66.67% CPU
      6,413,576 processor cycles
      0 bytes consed
    6050183040
    
    After:
    
    DAY-03> (let ((map (read-day-input 3 #'identity)))
               (time (count-collisions map)))
    Evaluation took:
      0.001 seconds of real time
      0.000074 seconds of total run time (0.000073 user, 0.000001 system)
      0.00% CPU
      186,700 processor cycles
      0 bytes consed
    
    6050183040

--- src/day03.lisp
+++ src/day03.lisp
@@ -2,27 +2,19 @@
   (:nicknames :day-03)
   (:use :cl)
   (:import-from :advent2020.util #:read-day-input)
-  (:import-from :split-sequence #:split-sequence)
   (:export #:count-trees #:count-collisions))
 
 (in-package :day-03)
 
-(defun parse-terrain (row)
-  "Represent terrain as a circular list extending infinitely."
-  (let ((items (coerce row 'list)))
-    (setf (cdr (last items)) items)
-    items))
-
 (defun collision? (terrain column)
-  (char= #\# (nth column terrain)))
+  (char= #\# (char terrain (mod column (length terrain)))))
 
 (defun count-trees (map &key (down 1) (right 3))
   (loop
-    for row = 0 then (+ row down)
+    for terrain on map by (alexandria:curry #'nthcdr down)
     for column = 0 then (+ column right)
-    for terrain = (nth row map)
-    while terrain
-    counting (collision? terrain column) into total
+    while (first terrain)
+    counting (collision? (first terrain) column) into total
     finally (return total)))
 
 (defun count-collisions (map)
@@ -32,9 +24,9 @@
     finally (return (reduce #'* counts))))
 
 (defun part-1 ()
-  (let ((map (read-day-input 3 #'parse-terrain)))
+  (let ((map (read-day-input 3 #'identity)))
     (count-trees map)))
 
 (defun part-2 ()
-  (let ((map (read-day-input 3 #'parse-terrain)))
+  (let ((map (read-day-input 3 #'identity)))
     (count-collisions map)))
