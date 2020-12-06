588f0ecf9782cfd6c9973963b8b11449b17853c2
Brit Butler <brit@kingcons.io>
2020-12-06 00:27:28 -0500

    Simplify. Lists perform similarly and cons ~600k less.
    Before:
    DAY-06> (time (part-1))
    Evaluation took:
      0.003 seconds of real time
      0.003555 seconds of total run time (0.002949 user, 0.000606 system)
      133.33% CPU
      1 form interpreted
      9,207,958 processor cycles
      1,125,856 bytes consed
    
    6726
    
    After:
    DAY-06> (time (part-1))
    
    Evaluation took:
      0.003 seconds of real time
      0.003557 seconds of total run time (0.003222 user, 0.000335 system)
      133.33% CPU
      1 form interpreted
      9,214,598 processor cycles
      590,768 bytes consed
    
    6726

--- src/day06.lisp
+++ src/day06.lisp
@@ -5,11 +5,8 @@
   (:export))
 
 (defun parse-group-any (group)
-  (loop with hash = (make-hash-table)
-        for char across group
-        when (alpha-char-p char)
-          do (setf (gethash char hash) char)
-        finally (return hash)))
+  (let ((answers (remove-if-not #'alpha-char-p (remove-duplicates group))))
+    (coerce answers 'list)))
 
 (defun parse-group-all (group)
   (let* ((entries (cl-ppcre:split "\\n" group))
@@ -18,7 +15,7 @@
 
 (defun part-1 ()
   (let ((groups (read-day-input 6 #'parse-group-any :separator "\\n\\n")))
-    (reduce #'+ (mapcar #'hash-table-count groups))))
+    (reduce #'+ (mapcar #'length groups))))
 
 (defun part-2 ()
   (let ((groups (read-day-input 6 #'parse-group-all :separator "\\n\\n")))
