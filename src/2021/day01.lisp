8a9d6c8b7b85a97d51075761e134f78ee72e2198
Brit Butler <brit@kingcons.io>
2021-12-02 07:13:19 -0500

    Add some additional variations and notes on Day 1.

--- src/2021/day01.lisp
+++ src/2021/day01.lisp
@@ -8,7 +8,8 @@
   (@part-1 section)
   (count-depths function)
   (@part-2 section)
-  (count-depths-sliding function))
+  (count-average-depths function)
+  (count-windowed-depths function))
 
 (defsection @part-1 (:title "Into the Depths")
   "For part 1, we'll just be checking how often a depth reading
@@ -29,16 +30,83 @@ in a sliding window three at a time. I'm still using a straightforward
 loop but the partitioning of the list is ugly. Two options for improvement are:
 
 1. Solve the problem in two passes, first generating sums then counting increases.
-2. Factor out the size of the window, either via callback or some other means.")
+2. Factor out the size of the window, either via callback or some other means.
 
-(defun count-depths-sliding (sonar-readings)
-  (loop with previous = (reduce #'+ (subseq sonar-readings 0 3))
-        for (first second third) on (subseq sonar-readings 1)
-        while third
-        for sum = (+ first second third)
-        count (> sum previous)
-        do (setf previous sum)))
+Interesting notes after more experiments. I've tried a number of different approaches
+to this problem, some that are flexible enough to accommodate both parts of the puzzle
+and some that are specialized to the 3-element window of the second part.
+
+Looking at the generic versions that can account for any input size we have 3 speed tiers:
+
+1. COUNT-DISPLACED-DEPTHS which is depressingly slow due to use of displaced arrays.
+   Neat feature that they are, it seems displaced arrays are just plain slow. ~5x slower.
+
+2. COUNT-WINDOWED-DEPTHS which uses REDUCE and LOOP to compute the result. ~2.5x-3 slower.
+
+3. COUNT-INCREASING-SUMS-LOOP which is a direct translation of death's solution using DO.
+   I often enjoy reviewing death's solutions as he tends to have different ideas than I do
+   and I think he has good taste and knowledge of the language. I don't much like DO though.
+
+It's worth pointing out that my COUNT-WINDOWED-DEPTHS is flexible enough to accept a list or array
+as an input. ...Though the performance degrades substantially from ~2.5x slower to 170x slower lol.
+This is due to repeated traversals of the input list by reduce.
+
+Notably, it was much easier to write a version using lists instead of arrays that conses _and_
+uses 2 passes but because it is specialized on a 3-element version, it is just as fast as the
+fastest generic version. Granted, I haven't tried it on larger inputs. COUNT-AVERAGE-DEPTHS is
+clean and simple but I was curious if I could eliminate the consing and go faster. After some
+experimentation, I wound up with COUNT-SHIFTING-DEPTHS which is 4-5x faster than
+COUNT-INCREASING-SUMS-LOOP! Very interesting to see how much of a difference
+specializing makes in this case.")
+
+(defun count-average-depths (sonar-readings)
+  (let ((sums (loop for (first second third) on sonar-readings
+                    while third collect (+ first second third))))
+    (loop for (previous current) on sums
+          while current count (> current previous))))
+
+(defun count-shifting-depths (readings)
+  (loop with (x y z) of-type ub32
+        with count of-type fixnum = 0 and previous of-type fixnum = 0
+        for depth in readings
+        do (progn
+             (shiftf x y z depth)
+             (when (> (+ x y z) previous)
+               (incf count))
+             (setf previous (+ x y z)))
+        finally (return (- count 3))))
+
+(defun count-windowed-depths (readings window-size)
+  (loop for i from 0 upto (- (length readings) window-size)
+        with prev = (reduce #'+ readings :start i :end (+ i window-size))
+        for sum = (reduce #'+ readings :start i :end (+ i window-size))
+        count (> sum prev)
+        do (setf prev sum)))
+
+(defun count-increasing-sums-loop (input k)
+  (loop for i below (- (length input) k)
+        with prev = (reduce #'+ input :end k)
+        for sum = (+ (- prev (aref input i))
+                     (aref input (+ i k)))
+        count (> sum prev)
+        do (setf prev sum)))
+
+(defun count-displaced-depths (readings window-size)
+  (let ((window (make-array window-size :displaced-to readings)))
+    (flet ((advance (offset)
+             (adjust-array window window-size :displaced-to readings :displaced-index-offset offset)))
+      (loop with previous = 0 and count = 0 and offset = 0
+            while (< offset (- (length readings) window-size))
+            do (let ((sum (reduce #'+ window)))
+                 (declare (fixnum sum previous count))
+                 (when (> sum previous)
+                   (incf count))
+                 (setf previous sum)
+                 (advance (incf offset)))
+            finally (return count)))))
 
 (defun part-2 ()
-  (let ((data (read-day-input #'parse-integer)))
-    (summarize (count-depths-sliding data))))
+  (let* ((data (read-day-input #'parse-integer))
+         (nums (coerce data 'vector)))
+    (declare (ignore nums))
+    (summarize (count-shifting-depths data))))
