f2890a5106585075b268ba63598c8ca02aedf67a
Brit Butler <brit@kingcons.io>
2021-12-05 15:48:40 -0500

    Minor tweaks to day 5.

--- src/2021/day05.lisp
+++ src/2021/day05.lisp
@@ -1,7 +1,6 @@
 (mgl-pax:define-package :aoc.2021.05
   (:nicknames :2021.05)
-  (:use :cl :aoc.util :mgl-pax)
-  (:import-from :alexandria :hash-table-values))
+  (:use :cl :aoc.util :mgl-pax))
 
 (in-package :2021.05)
 
@@ -11,20 +10,18 @@
 
 (defsection @part-1 (:title "Overlapping Vents"))
 
-(defstruct point
-  (x-pos 0 :type fixnum)
-  (y-pos 0 :type fixnum))
+(defstruct segment
+  x1 y1 x2 y2)
 
-(defun parse-point (line)
+(defun parse-segment (line)
   (cl-ppcre:register-groups-bind ((#'parse-integer x1 y1 x2 y2))
       ("(\\d+),(\\d+)\\s+->\\s+(\\d+),(\\d+)" line)
-    (list (make-point :x-pos x1 :y-pos y1)
-          (make-point :x-pos x2 :y-pos y2))))
+    (make-segment :x1 x1 :y1 y1 :x2 x2 :y2 y2)))
 
 (defun coordinate-match? (segment)
-  (destructuring-bind (p1 p2) segment
-    (or (= (point-x-pos p1) (point-x-pos p2))
-        (= (point-y-pos p1) (point-y-pos p2)))))
+  (with-slots (x1 y1 x2 y2) segment
+    (or (= x1 x2)
+        (= y1 y2))))
 
 (defun build-range (n1 n2 length)
   (cond ((= n1 n2) (loop repeat (1+ length) collect n1))
@@ -32,31 +29,27 @@
         ((> n1 n2) (loop for i from n1 downto n2 collect i))))
 
 (defun list-points (segment)
-  (destructuring-bind (p1 p2) segment
-    (let ((length (max (abs (- (point-x-pos p1) (point-x-pos p2)))
-                       (abs (- (point-y-pos p1) (point-y-pos p2))))))
+  (with-slots (x1 y1 x2 y2) segment
+    (let ((length (max (abs (- x1 x2))
+                       (abs (- y1 y2)))))
       (mapcar #'list
-              (build-range (point-x-pos p1) (point-x-pos p2) length)
-              (build-range (point-y-pos p1) (point-y-pos p2) length)))))
+              (build-range x1 x2 length)
+              (build-range y1 y2 length)))))
 
-(defun find-overlapping-vents (segments &key (skip-diagonal t))
-  (let ((valid-segments (if skip-diagonal
-                            (remove-if-not #'coordinate-match? segments)
-                            segments))
-        (grid (make-hash-table :test #'equal)))
-    (loop for i = 0 then (1+ i)
-          for segment in valid-segments
-          do (dolist (point (list-points segment))
-               (let ((seen (gethash point grid 0)))
-                 (setf (gethash point grid) (1+ seen))))
-          finally (return (count-if (lambda (x) (> x 1)) (hash-table-values grid))))))
+(defun find-overlapping-vents (segments &optional (count 0) (grid (make-hash-table :test #'equal)))
+  (loop for segment in segments
+        do (dolist (point (list-points segment))
+             (let ((seen (incf (gethash point grid 0))))
+               (when (= seen 2)
+                 (incf count))))
+        finally (return (values count grid))))
 
 (defun part-1 ()
-  (let ((segments (read-day-input #'parse-point)))
-    (summarize (find-overlapping-vents segments))))
+  (let ((segments (read-day-input #'parse-segment)))
+    (summarize (find-overlapping-vents (remove-if-not #'coordinate-match? segments)))))
 
 (defsection @part-2 (:title "Diagonal Overlap"))
 
 (defun part-2 ()
-  (let ((segments (read-day-input #'parse-point)))
-    (summarize (find-overlapping-vents segments :skip-diagonal nil))))
+  (let ((segments (read-day-input #'parse-segment)))
+    (summarize (find-overlapping-vents segments))))
