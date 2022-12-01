78a756982c5fa57138f4617dac1fc20ef0570c1c
Brit Butler <brit@kingcons.io>
2022-12-01 08:39:18 -0500

    Refactor READ-DAY-INPUT to allow mocking input data.
    Extracting the logic to determine the file path for a day
    based on the current PACKAGE and read it in makes it trivial
    to prefer explicit input if supplied.

--- src/util.lisp
+++ src/util.lisp
@@ -1,22 +1,28 @@
 (defpackage :aoc.util
   (:use :cl)
+  (:import-from :alexandria #:read-file-into-string)
+  (:import-from :cl-ppcre #:split)
+  (:import-from :serapeum #:~>>)
   (:export #:read-day-input #:summarize))
 
 (in-package :aoc.util)
 
-(defmacro read-day-input (item-parser &key (separator "\\n") (whole nil))
-  "Load the input data for the current day based on the name of *PACKAGE*, splitting
-the file based on the value of SEPARATOR and processing each string with ITEM-PARSER.
-If WHOLE is non-nil, after splitting pass to ITEM-PARSER directly instead of mapping."
+(defun read-dat-file-for-package ()
   (cl-ppcre:register-groups-bind (year day)
       ("(\\d{4})\.(\\d{2})" (package-name *package*))
-    `(serapeum:~>> (format nil "src/~d/day~d.dat" ,year ,day)
-                   (asdf:system-relative-pathname :advent)
-                   (alexandria:read-file-into-string)
-                   (cl-ppcre:split ,separator)
-                   ,(if whole
-                        `(funcall ,item-parser)
-                        `(mapcar ,item-parser)))))
+    (let ((pathname (format nil "src/~d/day~d.dat" year day)))
+      (read-file-into-string (asdf:system-relative-pathname :advent pathname)))))
+
+(defmacro read-day-input (item-parser &key (separator "\\n") (whole nil) (input nil))
+  "Load the input data for the current day based on the name of *PACKAGE*, splitting
+the file based on the value of SEPARATOR and processing each string with ITEM-PARSER.
+If INPUT is supplied, use that instead of loading the DAT file matching the *PACKAGE*.
+If WHOLE is non-nil, after splitting pass to ITEM-PARSER directly instead of mapping."
+  (let ((data (or input (read-dat-file-for-package))))
+    `(~>> (split ,separator ,data)
+          ,(if whole
+               `(funcall ,item-parser)
+               `(mapcar ,item-parser)))))
 
 (defmacro summarize (form)
   "Measure the real time to execute FORM and return a string showing the result
