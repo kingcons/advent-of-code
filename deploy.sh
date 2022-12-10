adb7b0c89c8b1ff487f43d5129512678185b384a
Brit Butler <brit@kingcons.io>
2022-12-09 22:41:13 -0500

    An ugly hack to fix our deploy script.
    The deploy script is failing because the functions we want to SUMMARIZE
    haven't been compiled at the time the macro is expanded so we just get
    NIL. Beat this at deploy time by forcefully compiling the entire system
    with ASDF so that the sections have valid data when we run BUILD-SITE. 😓

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 git branch -D gh-pages
 git checkout -b gh-pages
-sbcl --eval "(ql:quickload :advent)" --eval "(aoc::build-site)" --quit
+sbcl --non-interactive \
+     --eval "(ql:quickload :advent)" \
+     --eval "(asdf:compile-system :advent :force t)" \
+     --eval "(aoc::build-site)" --quit
 ln -sf advent.html docs/index.html
 git add -f docs/
 git commit -m "Generate site."
